1996d56c875eeba42960d1bcd13cef22da278612
karaage0703 <coldchicken@gmail.com>
2018-08-10 02:05:43 +0900

    Add shutdown function

--- food_diary.py
+++ food_diary.py
@@ -10,56 +10,71 @@ from gpiozero import Button
 from time import sleep
 import picamera
 from slacker import Slacker
+import time
+import os
 
 BUZZER_GPIO = 22
 BUTTON_GPIO = 23
 
-BEEP_SOUND = ('E6q', 'C6q')
-
-photo_filename = 'dish.jpg'
-
 slack = Slacker('xxx')
 toneplayer = TonePlayer(BUZZER_GPIO)
 leds = Leds()
+button = Button(BUTTON_GPIO)
 
+photo_filename = 'dish.jpg'
+BEEP_SOUND = ('E6q', 'C6q')
 RED = (0xFF, 0x00, 0x00)
 GREEN = (0x00, 0xFF, 0x00)
 BLUE = (0x00, 0x00, 0xFF)
 WHITE = (0xFF, 0xFF, 0xFF)
 
+def KeepWatchForSeconds(seconds):
+    GoFlag = True
+    while seconds > 0:
+        time.sleep(0.1)
+        seconds -= 0.1
+        if not button.is_pressed:
+            GoFlag = False
+            break
+    return GoFlag
+
 def run():
-    print("beep sound")
-    toneplayer.play(*BEEP_SOUND)
+    if KeepWatchForSeconds(3):
+        print("Going shutdown by GPIO")
+        leds.update(Leds.rgb_off())
+        os.system("/sbin/shutdown -h now 'Poweroff by GPIO'")
 
-    leds.update(Leds.rgb_on(RED))
-    print("take photo")
-    with picamera.PiCamera() as camera:
-        camera.resolution = (640, 480)
-        camera.start_preview()
-        sleep(3.000)
-        camera.capture(photo_filename)
+    else:
+        print("Beep sound")
+        toneplayer.play(*BEEP_SOUND)
 
-    leds.update(Leds.rgb_on(GREEN))
-    print("dish classifier")
-    with ImageInference(dish_classifier.model()) as inference:
-        image = Image.open(photo_filename)
-        classes = dish_classifier.get_classes(
-            inference.run(image), max_num_objects=5, object_prob_threshold=0.1)
-        dish_name = ''
-        for i, (label, score) in enumerate(classes):
-            dish_name += label + '/'
-            print('Result %d: %s (prob=%f)' % (i, label, score))
+        leds.update(Leds.rgb_on(RED))
+        print("Taking photo")
+        with picamera.PiCamera() as camera:
+            camera.resolution = (640, 480)
+            camera.start_preview()
+            sleep(3.000)
+            camera.capture(photo_filename)
 
-    leds.update(Leds.rgb_on(BLUE))
-    print("post to slack")
-    slack.files.upload(photo_filename, channels='#dish', title=dish_name)
+        leds.update(Leds.rgb_on(GREEN))
+        print("Dish classifier")
+        with ImageInference(dish_classifier.model()) as inference:
+            image = Image.open(photo_filename)
+            classes = dish_classifier.get_classes(
+                inference.run(image), max_num_objects=5, object_prob_threshold=0.1)
+            dish_name = ''
+            for i, (label, score) in enumerate(classes):
+                dish_name += label + '/'
+                print('Result %d: %s (prob=%f)' % (i, label, score))
 
-    leds.update(Leds.rgb_on(WHITE))
+        leds.update(Leds.rgb_on(BLUE))
+        print("Post to slack")
+        slack.files.upload(photo_filename, channels='#food_diary', title=dish_name)
+
+        leds.update(Leds.rgb_on(WHITE))
 
 def main():
-    button = Button(BUTTON_GPIO)
     button.when_pressed = run
-
     leds.update(Leds.rgb_on(WHITE))
 
     try:
@@ -70,5 +85,5 @@ def main():
         pass
 
 if __name__ == '__main__':
-    print("start program")
+    print("Start program")
     main()
